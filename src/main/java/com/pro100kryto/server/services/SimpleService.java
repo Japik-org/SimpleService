57c242f72025adae893de8cf59afff3d83403d64
pro100kryto <pro100kryto.original@gmail.com>
2020-12-25 19:17:51 -0300

    Create project and implement

---
+++ src/main/java/com/pro100kryto/server/services/SimpleService.java
@@ -0,0 +1,17 @@
+package com.pro100kryto.server.services;
+
+import com.pro100kryto.server.service.AServiceType;
+import com.pro100kryto.server.service.Service;
+import com.pro100kryto.server.services.simple.ISimpleServiceConnection;
+
+public class SimpleService extends AServiceType<ISimpleServiceConnection> {
+
+    public SimpleService(Service service) {
+        super(service);
+    }
+
+    @Override
+    protected ISimpleServiceConnection createServiceConnection() {
+        return null;
+    }
+}
---
+++ src/main/java/com/pro100kryto/server/services/simple/ISimpleServiceConnection.java
@@ -0,0 +1,6 @@
+package com.pro100kryto.server.services.simple;
+
+import com.pro100kryto.server.service.IServiceConnection;
+
+public interface ISimpleServiceConnection extends IServiceConnection {
+}

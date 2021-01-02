e9d94847dedd6f4aeff9818f169221c2292709e6
pro100kryto <pro100kryto.original@gmail.com>
2021-01-02 18:30:16 -0300

    Implement missing ServiceConnection

--- src/main/java/com/pro100kryto/server/services/SimpleService.java
+++ src/main/java/com/pro100kryto/server/services/SimpleService.java
@@ -4,6 +4,8 @@ import com.pro100kryto.server.service.AServiceType;
 import com.pro100kryto.server.service.Service;
 import com.pro100kryto.server.services.simple.connection.ISimpleServiceConnection;
 
+import java.rmi.RemoteException;
+
 public class SimpleService extends AServiceType<ISimpleServiceConnection> {
 
     public SimpleService(Service service) {
@@ -12,6 +14,14 @@ public class SimpleService extends AServiceType<ISimpleServiceConnection> {
 
     @Override
     protected ISimpleServiceConnection createServiceConnection() {
-        return null;
+        return new SimpleServiceConnection();
+    }
+
+    private final class SimpleServiceConnection implements ISimpleServiceConnection{
+
+        @Override
+        public boolean ping() throws RemoteException {
+            return true;
+        }
     }
 }

4c783cb18b7870caae32acc7208391a2210de8be
pro100kryto <pro100kryto.original@gmail.com>
2021-03-15 02:53:22 -0300

    Update for Server 0.2.0

--- src/main/java/com/pro100kryto/server/services/SimpleService.java
+++ src/main/java/com/pro100kryto/server/services/SimpleService.java
@@ -4,8 +4,6 @@ import com.pro100kryto.server.service.AServiceType;
 import com.pro100kryto.server.service.Service;
 import com.pro100kryto.server.services.simple.connection.ISimpleServiceConnection;
 
-import java.rmi.RemoteException;
-
 public class SimpleService extends AServiceType<ISimpleServiceConnection> {
 
     public SimpleService(Service service) {
@@ -20,7 +18,7 @@ public class SimpleService extends AServiceType<ISimpleServiceConnection> {
     private final class SimpleServiceConnection implements ISimpleServiceConnection{
 
         @Override
-        public boolean ping() throws RemoteException {
+        public boolean ping() {
             return true;
         }
     }
